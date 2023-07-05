3d650a967f21a55c5d165302371c58c4285ea4ea
Gokul-H7 <gokulh2025@gmail.com>
2023-07-05 17:06:44 -0400

    terminal window size handling

--- scallop.c
+++ scallop.c
@@ -2,28 +2,41 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+// CONFIG
 
-void terminate(const char *s) {
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+// TERMINAL
+void terminate(const char *s) { 
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) terminate("tcsetattr");
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) terminate("tcsetattr");
 }
 
 void enableRawMode() {
-  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) terminate("tcgetattr");
+  if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) terminate("tcgetattr");
   atexit(disableRawMode);
 
   // disable ctrl key signals
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_cflag &= (CS8);
   raw.c_oflag &= ~(OPOST);
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -43,20 +56,81 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  printf("\r\n");
+  char c;
+
+  while(read(STDIN_FILENO, &c, 1) == 1){
+    if(iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+
+  editorReadKey();
+  return -1;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+// INPUT
+
 void editorProcessKeypress() {
   char c = editorReadKey();
 
   switch(c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      
       exit(0);
       break;
   }
 }
 
+// OUTPUT
+
+void editorDrawRows() {
+  int y;
+  for(y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+// INIT
+
+void initEditor() {
+  if(getWindowSize(&E.screenrows, &E.screencols) == -1) terminate("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while(1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
